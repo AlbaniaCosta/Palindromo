2f0d201af9bdcf6b6846b2e2bcc54b3efcf565b2
jacelia <jacelia.costa@gmail.com>
2024-08-25 15:34:42 -0300

    primeiro commit

---
+++ palindromo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace palindromo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Digite uma palavra: ");
+            
+            string palavra = Console.ReadLine();
+
+            palavra = palavra.Replace(" ", "").ToLower();
+
+            int tamanho = palavra.Length - 1, verificador = 0;
+
+            for (int i = 0; i < palavra.Length; i++, tamanho--)
+
+            {
+
+                if (palavra[i] == palavra[tamanho])
+
+                {
+
+                    verificador++;
+
+                }
+
+            }
+
+            if (verificador == palavra.Length)
+
+            {
+
+                Console.WriteLine("É palíndromo");
+
+            }
+
+            else
+
+            {
+
+                Console.WriteLine("Não é palindromo");
+
+            }
+        }
+    }
+}
